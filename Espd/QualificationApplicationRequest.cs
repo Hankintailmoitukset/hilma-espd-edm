dc6bb6f3b43ee6fde2c5ea4bafea2544011b4ef0
Tuomas Tolvanen <tuomas.tolvanen@innofactor.fi>
2020-05-12 18:18:08 +0300

    Added legislation mapper and property inintializer

--- Espd/QualificationApplicationRequest.cs
+++ Espd/QualificationApplicationRequest.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Hilma.Espd.EDM.CriterionModels;
+using Hilma.Espd.EDM.CriterionModels.v2_1_0.Identifiers;
 using Hilma.UBL.Attributes;
 using Hilma.UBL.CommonAggregateComponents;
 using Hilma.UBL.CommonExtensionComponents;
@@ -169,6 +173,28 @@ namespace Hilma.Espd.EDM
     /// </summary>
     /// <remarks>For procurement procedures above the threshold it is compulsory to make reference to the Contract Notice of the procedure published in TED. See section "Reference to the Contract Notice" for a complete example.</remarks>
     public AdditionalDocumentReference[] AdditionalDocumentReferences { get; set; }
+    
+    /// <summary>
+    /// Finalize document properties
+    /// </summary>
+    public void FinalizeDocument()
+    {
+      void FinalizeGroup(IEnumerable<TenderingCriterionPropertyGroup> groups)
+      {
+        foreach (var group in groups)
+        {
+          foreach (var property in group.TenderingCriterionProperties)
+          {
+            property.Id = EuComGrowId.Random();
+          }
+          FinalizeGroup(group.SubsidiaryTenderingCriterionPropertyGroups);
+        }
+      }
 
+      foreach (var criterion in TenderingCriteria)
+      {
+        FinalizeGroup(criterion.TenderingCriterionPropertyGroups);
+      }
+    }
   }
 }
---
+++ Espd/Utilities/LegislationFactory.cs
@@ -0,0 +1,61 @@
+﻿using Hilma.Espd.EDM.CriterionModels.v2_1_0.Identifiers;
+using Hilma.UBL.CommonAggregateComponents;
+
+namespace Hilma.Espd.EDM.Utilities
+{
+  public class LegislationFactory
+  {
+      /// <summary>
+      /// Directive 2014/24/EU on public procurement
+      /// </summary>
+      public const string EuEuratom2018Directive = "32018R1046";
+
+      /// <summary>
+      /// Directive (EU, Euratom) N:o 2018/1046
+      /// </summary>
+      public const string EuPublicProcurements2014Directive = "32014L0024";
+
+      /// <summary>
+      /// Directive 2014/25/EU on procurement by entities operating in the water, energy, transport and postal services sectors.
+      /// </summary>
+      public const string EuUtilitiesProcurements2014Directive = "32014L0025";
+
+      /// <summary>
+      /// Directive 2014/23/EU Concession notices
+      /// </summary>
+      public const string EuConcessionProcurement2014Directive = "32014L0023";
+
+      /// <summary>
+      /// Directive 2009/81/EC Defence contracts
+      /// </summary>
+      public const string EuDefenceProcurements2009Directive = "32009L0081";
+
+      public Legislation CreateFromDirective( string directive )
+      {
+        switch (directive)
+        {
+          case EuPublicProcurements2014Directive:
+            
+            return new Legislation()
+            {
+              ID = new EuComGrowId("4ea7a10a-643e-4022-b67e-e06573b28ff5"),
+              Titles = new[] { "DIRECTIVE 2014/24/EU OF THE EUROPEAN PARLIAMENT AND OF THE COUNCIL of 26 February 2014 on public procurement and repealing Directive 2004/18/EC" },
+              Descriptions = new []
+              {
+                "DIRECTIVE 2014/24/EU OF THE EUROPEAN PARLIAMENT AND OF THE COUNCIL of 26 February 2014 on public procurement and repealing Directive 2004/18/EC"
+              },
+              JurisdictionLevels = new []{ "EU_DIRECTIVE"},
+              Articles = new [] {"57(1)" },
+              
+
+
+            break;
+        }  
+
+
+
+      }
+
+
+  }
+}
