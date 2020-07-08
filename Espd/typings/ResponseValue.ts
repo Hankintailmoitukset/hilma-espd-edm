764b6049ac9129fe597158ba213d8501323713d4
Sami Pöysä <sami.poysa@innofactor.com>
2020-07-08 09:12:10 +0300

    fixed responseValue

---
+++ Espd/typings/ResponseValue.ts
@@ -0,0 +1,32 @@
+﻿
+
+
+import { IdentifierType } from './IdentifierType'
+import { AmountType } from './AmountType'
+import { CodeType } from './CodeType'
+import { DateType } from './DateType'
+import { TimeType } from './TimeType'
+import { MeasureType } from './MeasureType'
+import { QuantityType } from './QuantityType'
+
+import assign from 'lodash.assign'
+
+export class ResponseValue { 
+    public constructor(init?:Partial<ResponseValue>) {
+        assign(this, init)
+    } 
+    
+    
+    id?: IdentifierType
+    description?: string[]
+    responseAmount?: AmountType
+    responseCode?: CodeType
+    responseDate?: DateType
+    responseTime?: TimeType
+    responseID?: IdentifierType
+    responseIndicator?: boolean
+    responseMeasure?: MeasureType
+    responseNumeric?: number
+    responseQuantity?: QuantityType
+    responseURI?: IdentifierType
+}
--- Espd/typings/TenderingCriterionResponse.ts
+++ Espd/typings/TenderingCriterionResponse.ts
@@ -5,6 +5,7 @@ import { IdentifierType } from './IdentifierType'
 import { CodeType } from './CodeType'
 import { PeriodType } from './PeriodType'
 import { EvidenceType } from './EvidenceType'
+import { ResponseValue } from './ResponseValue'
 
 
 import assign from 'lodash.assign'
