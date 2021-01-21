1da34268c24e2bee8727cc06e009d8b0b84fa780
Sami Pöysä <sami.poysa@innofactor.com>
2021-01-21 14:27:27 +0200

    removed mandatoryies

--- Espd/CriterionModels/v2_1_1/OtherCriteriaSpecification.cs
+++ Espd/CriterionModels/v2_1_1/OtherCriteriaSpecification.cs
@@ -322,7 +322,7 @@ namespace Hilma.Espd.EDM.CriterionModels.v2_1_1
                                   },
                                   new TenderingCriterionProperty()
                                   {
-                                    _cardinality = CardinalityMetadata.ExactlyOne,
+                                    _cardinality = CardinalityMetadata.Optional,
                                     ID = EuComGrowId.Random(),
                                     TypeCode = CriterionElementType.Question,
                                     Description = "common.iDOfTheEntity.description",
