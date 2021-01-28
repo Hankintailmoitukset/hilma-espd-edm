9c0302e0d5cf6829fa1212fa469be8d709d7783b
Alex Engblom <alex.engblom@innofactor.com>
2021-01-28 15:25:39 +0200

    Remove model binder validation because we want to allow saving for drafts.

--- UBL/CommonAggregateComponents/PartyName.cs
+++ UBL/CommonAggregateComponents/PartyName.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 using Hilma.UBL.Attributes;
 using Hilma.UBL.Serializers;
 
@@ -14,8 +13,6 @@ namespace Hilma.UBL.CommonAggregateComponents
     /// <summary>
     /// Mandatory. Use the official name of the contracting body. The PartyName class has an associated basic element "cbc:Name". See XML example below.
     /// </summary>
-    /// <remarks>Mandatory. Use the official name of the contracting body.</remarks>
-    [Required]
     public string Name { get; set; }
 
     public XElement Serialize()
